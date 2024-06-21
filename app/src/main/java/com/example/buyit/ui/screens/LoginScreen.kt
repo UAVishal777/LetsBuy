75eb607381125d754ec53c0014cac2d7ed0ad67c
Manish Prajapati <Manish.Prajapati@ibm.com>
2024-06-22 01:24:30 +0530

    Added the Login Screen Preview

--- app/src/main/java/com/example/buyit/ui/screens/LoginScreen.kt
+++ app/src/main/java/com/example/buyit/ui/screens/LoginScreen.kt
@@ -1,7 +1,18 @@
 package com.example.buyit.ui.screens
 
+import android.content.res.Configuration
 import android.util.Log
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
+@Composable
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_NO or Configuration.UI_MODE_TYPE_NORMAL,
+    backgroundColor = 0xFFFFFFFF
+)
 fun LoginScreen(){
-    Log.d("Test-0.0.1 ==> ", "LoginScreen: ")
+    Column {
+        Text(text = "Hi")
+    }
 }
