0d4479d46a9a7314988460faca4b8f298b8ec963
Manish Prajapati <Manish.Prajapati@ibm.com>
2024-06-22 02:12:31 +0530

    Added some changes to the LoginScreen

--- app/src/main/java/com/example/buyit/MainActivity.kt
+++ app/src/main/java/com/example/buyit/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.buyit.ui.screens.Login.LoginScreen
 import com.example.buyit.ui.theme.BuyItTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,12 +17,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BuyItTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Krishna",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                LoginScreen()
             }
         }
     }
